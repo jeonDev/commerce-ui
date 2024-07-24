80dc9344692204c993b032079f4fa90b70a31a6e
jeonDev <jjhwhdgus0727@gmail.com>
2024-07-24 21:57:50 +0900

    feat ( order ) : table 추가 (back 값 변경 필요)

--- src/components/admin/AdminOrderList.vue
+++ src/components/admin/AdminOrderList.vue
@@ -1,6 +1,29 @@
 <template>
   <div>
-    {{response}}
+    <b-table-simple>
+      <b-thead>
+        <b-tr>
+          <b-th>No.</b-th>
+          <b-th>상품</b-th>
+          <b-th>금액</b-th>
+          <b-th>납부금액</b-th>
+          <b-th>상태</b-th>
+        </b-tr>
+      </b-thead>
+      <b-tbody>
+        <b-tr
+            v-for="(item, idx) in response.list"
+        >
+          <b-td>{{idx + 1}}</b-td>
+          <b-td>
+            {{item.orderDetailInfos[0].productName}} 외 {{item.orderDetailInfos.length}}건
+          </b-td>
+          <b-td>{{item.buyAmount}}</b-td>
+          <b-td>{{item.paidAmount}}</b-td>
+          <b-td>{{item.orderStatus}}</b-td>
+        </b-tr>
+      </b-tbody>
+    </b-table-simple>
   </div>
 </template>
 
